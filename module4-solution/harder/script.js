6db642b7831b93ec019d69c9ccf1384cef936f06
alemos2611 <alemos2611@yahoo.com>
2019-05-11 17:46:31 -0400

    Update script.js

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -58,8 +58,8 @@ for (var i=0; i< names.length; i++ /* fill in parts of the 'for' loop to loop ov
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    byeSpeaker.speak(name[i]);
+    byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak(name[i]);
+    helloSpeaker.speak(names[i]);
   }
 }
